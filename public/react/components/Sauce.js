41229280014ff211f20918a61248ac460fa26eee
Aaron Mansfield <aaronmansfield12@gmail.com>
2023-11-21 15:07:37 +0000

    React Update

--- public/react/components/Sauce.js
+++ public/react/components/Sauce.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Sauce = (props) => {
 
   return <>
-    <div className='container'>
+    <div>
         <h3>{props.item.name}</h3>
         <img src={props.item.image} alt={props.item.name} />
     </div>
