b889ea73aae467ffb144bacd86c0de76a6c044e6
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-11-24 11:45:27 +0000

    bugs fixed

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -3,10 +3,10 @@ import { Item } from "./Item";
 
 export const ItemsList = ({ items }) => {
   return (
-    <>
+    <div className="container">
       {items.map((item, idx) => {
         return <Item item={item} key={idx} />;
       })}
-    </>
+    </div>
   );
 };
